748bae0ee0c4dac70ef7146c1b31c758641159ee
César Dalisay <cesartrbj@gmail.com>
2025-04-10 20:01:54 +0200

    Estos son los archivos iniciales.
    En el archivo CSS podemos cambiar los estilos.

---
+++ clockscript.js
@@ -0,0 +1,22 @@
+function updateClock() {
+    const now = new Date();
+
+    //Obtener fecha.
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const year = now.getFullYear();
+
+    //Obtener hora.
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById('date').textContent = `${day}/${month}/${year}`;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+setInterval(updateClock, 1000); //1000 = 1 segundo.
+
+updateClock();
